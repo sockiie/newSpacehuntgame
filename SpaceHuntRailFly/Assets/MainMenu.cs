9d266b037056e6e0f7d6f5a6d511d086e443495c
sockiie <68150333+sockiie@users.noreply.github.com>
2021-06-20 12:40:46 +0200

    test
    test

--- SpaceHuntRailFly/Assets/MainMenu.cs
+++ SpaceHuntRailFly/Assets/MainMenu.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
+
+    
 {
-   public void PlayGame ()
+    
+    public void PlayGame ()
     {
+       
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         
     }
